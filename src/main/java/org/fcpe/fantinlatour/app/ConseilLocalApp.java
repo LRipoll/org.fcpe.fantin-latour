4cb2f00daea73e3b35cf8bacbbd392df241c0b35
LRipoll <laurent.ripoll.dev@free.fr>
2018-04-18 23:00:56 +0200

    Dernière correction UTF 8

--- src/main/java/org/fcpe/fantinlatour/app/ConseilLocalApp.java
+++ src/main/java/org/fcpe/fantinlatour/app/ConseilLocalApp.java
@@ -49,8 +49,7 @@ public class ConseilLocalApp extends Application {
 				.getService(ConseilLocalEtablissementManager.ID);
 		conseilLocalEtablissementManager.init();
 		stage.show();
-		// TODO Afficher la fenêtre du mot de passe en fonction du fait qu'il y ait ou non conseil par défaut
-		// TODO Ajouter un test sur les CombinedPasswordValidator.
+		// TODO Afficher la feÃªtre du mot de passe en fonction du fait qu'il y ait ou non conseil par dÃ©faut
 	}
 
 }
