9853f7260369fb34899aa6089f164190164c7b03
LRipoll <laurent.ripoll.dev@free.fr>
2018-10-01 22:58:07 +0200

    Mise en place de l'IHM de configuration du serveur SMTP

--- src/main/java/org/fcpe/fantinlatour/app/controller/ConfigureEmailAccountController.java
+++ src/main/java/org/fcpe/fantinlatour/app/controller/ConfigureEmailAccountController.java
@@ -3,12 +3,16 @@ package org.fcpe.fantinlatour.app.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.fcpe.fantinlatour.app.controller.validator.DomainValidator;
 import org.fcpe.fantinlatour.app.controller.validator.EmailValidator;
+import org.fcpe.fantinlatour.app.controller.validator.IntegerValidator;
+import org.fcpe.fantinlatour.app.controller.validator.MandatoryListValidator;
 import org.fcpe.fantinlatour.app.controller.validator.PasswordValidator;
 import org.fcpe.fantinlatour.model.EmailSenderProtocol;
 import org.fcpe.fantinlatour.model.controller.ConseilLocalEtablissementManager;
 import org.fcpe.fantinlatour.service.SpringFactory;
 
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -19,10 +23,18 @@ import javafx.scene.control.TextField;
 
 public class ConfigureEmailAccountController extends AbstractController {
 
+	private static final int PORT_MAX = 65536;
+	private static final int PORT_MIN = 0;
 	static final String EMAIL_INVALID = "org.fcpe.fantinlatour.view.configureemailaccount.email.tooltip.invalid";
 	static final String EMAIL_VALID = "org.fcpe.fantinlatour.view.configureemailaccount.email.tooltip.valid";
 	static final String PASSWORD_INVALID = "org.fcpe.fantinlatour.view.configureemailaccount.password.tooltip.invalid";
 	static final String PASSWORD_VALID = "org.fcpe.fantinlatour.view.configureemailaccount.password.tooltip.valid";
+	static final String PROTOCOL_SELECTED = "org.fcpe.fantinlatour.view.configureemailaccount.protocol.tooltip.valid";
+	static final String PROTOCOL_UNSELECTED = "org.fcpe.fantinlatour.view.configureemailaccount.protocol.tooltip.invalid";
+	static final String HOST_VALID = "org.fcpe.fantinlatour.view.configureemailaccount.host.tooltip.valid";
+	static final String HOST_INVALID = "org.fcpe.fantinlatour.view.configureemailaccount.host.tooltip.invalid";
+	static final String PORT_VALID = "org.fcpe.fantinlatour.view.configureemailaccount.port.tooltip.valid";
+	static final String PORT_INVALID = "org.fcpe.fantinlatour.view.configureemailaccount.port.tooltip.invalid";
 	
 	protected ConseilLocalEtablissementManager conseilLocalEtablissementManager;
 
@@ -34,6 +46,10 @@ public class ConfigureEmailAccountController extends AbstractController {
 	private ComboBox<EmailSenderProtocol> protocolComboBox;
 	@FXML
 	private ObservableList<EmailSenderProtocol> listProtocol;
+	@FXML
+	private TextField hostTextField;
+	@FXML
+	private TextField portTextField;
 
 	public ConfigureEmailAccountController() {
 		super();
@@ -51,6 +67,13 @@ public class ConfigureEmailAccountController extends AbstractController {
 				resources.getString(PASSWORD_VALID), resources.getString(PASSWORD_INVALID)));
 		listProtocol = FXCollections.observableArrayList(EmailSenderProtocol.values());
 		protocolComboBox.setItems(listProtocol);
+		MandatoryListValidator<EmailSenderProtocol> mandatoryListValidator = new MandatoryListValidator<EmailSenderProtocol>(
+				sceneValidator, protocolComboBox, resources.getString(PROTOCOL_SELECTED), resources.getString(PROTOCOL_UNSELECTED));
+		protocolComboBox.valueProperty().addListener((ChangeListener<? super EmailSenderProtocol>) mandatoryListValidator);
+		hostTextField.textProperty().addListener(new DomainValidator(sceneValidator, hostTextField,
+				resources.getString(HOST_VALID), resources.getString(HOST_INVALID)));
+		portTextField.textProperty().addListener(new IntegerValidator(sceneValidator, portTextField,
+				resources.getString(PORT_VALID), resources.getString(PORT_INVALID),PORT_MIN,PORT_MAX));
 
 	}
 }
